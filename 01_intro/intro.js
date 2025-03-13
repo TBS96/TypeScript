74d793ba3da40ac512c888cfb9cc9f1f50149ff4
TBS96 <9tbs6@proton.me>
2025-03-13 16:37:10 +0530

    Add introductory TypeScript file with user object example
    Signed-off-by: TBS96 <9tbs6@proton.me>

---
+++ 01_intro/intro.js
@@ -0,0 +1,4 @@
+var user = { name: 'Prantik', age: 29 };
+console.log('Prantik');
+console.log(user.name);
+// console.log(user.email);    // Error: Property 'email' does not exist on type '{ name: string; age: number; }'.
---
+++ 01_intro/intro.ts
@@ -0,0 +1,5 @@
+const user = {name: 'Prantik', age: 29};
+
+console.log('Prantik');
+console.log(user.name);
+// console.log(user.email);    // Error: Property 'email' does not exist on type '{ name: string; age: number; }'.
