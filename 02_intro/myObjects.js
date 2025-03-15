160f6bd0524114476efef63a6a1cf5d60d171ae1
TBS96 <9tbs6@proton.me>
2025-03-15 21:09:19 +0530

    Add TypeScript types for User1 and demonstrate readonly properties and optional fields
    Intersection types using '&'. also known as combined types.
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- 02_intro/myObjects.js
+++ 02_intro/myObjects.js
@@ -23,3 +23,16 @@ function createUser1(user) {
     return { name: '', email: '', isActive: true };
 }
 createUser1({ name: '', email: '', isActive: true });
+var myUser = {
+    _id: '123',
+    name: 'prantik',
+    email: '9tbs6@proton.me',
+    isActive: true,
+    hobbies: ['coding', 'gaming', 'tweaking'],
+    // creditCardDetails: 1234567897564125
+};
+// myUser._id = '111';     // Cannot assign to '_id' because it is a read-only property
+myUser.hobbies.push('sleeping'); //hobbies is an [], so we can push values in case of READONLY
+myUser.email = 'hello@mello.com';
+var _id = myUser._id, name = myUser.name, email = myUser.email, isActive = myUser.isActive, hobbies = myUser.hobbies, creditCardDetails = myUser.creditCardDetails;
+console.log("My ID is ".concat(_id, ", my name is ").concat(name, ", my email is ").concat(email, " and am i logged in = ").concat(isActive, ". My hobbies are: ").concat(hobbies, ". My creadit card details are ").concat(creditCardDetails));
--- 02_intro/myObjects.ts
+++ 02_intro/myObjects.ts
@@ -44,4 +44,53 @@ createUser1({name: '', email: '', isActive: true})
 
 
 
+// ------------------------------------------------------------------------------------------------------------------------------
+
+
+// ------------------------------------------ READONLY & Optional ------------------------------------------
+
+
+type User1 = {
+    readonly _id: string,
+    name: string,
+    email: string,
+    isActive: boolean,
+    readonly hobbies: [string, string, string],
+    creditCardDetails?: number
+};
+
+let myUser: User1 = {
+    _id: '123',
+    name: 'prantik',
+    email: '9tbs6@proton.me',
+    isActive: true,
+    hobbies: ['coding', 'gaming', 'tweaking'],
+    // creditCardDetails: 1234567897564125
+};
+// myUser._id = '111';     // Cannot assign to '_id' because it is a read-only property
+myUser.hobbies.push('sleeping');    //hobbies is an [], so we can push values in case of READONLY
+myUser.email = 'hello@mello.com';
+
+const { _id, name, email, isActive, hobbies, creditCardDetails } = myUser;
+console.log(`My ID is ${_id}, my name is ${name}, my email is ${email} and am i logged in = ${isActive}. My hobbies are: ${hobbies}. My creadit card details are ${creditCardDetails}`);
+
+
+
+type cardNumber = {
+    cardnumber: string
+};
+
+type cardDate = {
+    carddate: string
+};
+
+// combining different data types using '&'
+type cardDetails = cardNumber & cardDate & {
+    cvv: number
+};
+
+// here, cardDetails is of a combined type, known as Intersections.
+
+
+
 export {}
