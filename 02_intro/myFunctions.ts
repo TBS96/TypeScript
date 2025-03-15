389d7bc3fb8273015a838eecf2001322f162e2cd
TBS96 <9tbs6@proton.me>
2025-03-15 17:34:22 +0530

    Fix return type of addTwo function to number and update comments for clarity
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- 02_intro/myFunctions.ts
+++ 02_intro/myFunctions.ts
@@ -1,6 +1,6 @@
-function addTwo(num: number) {
-    // return num + 2;
-    return 'hello';
+function addTwo(num: number): number {
+    return num + 2;
+    // return 'hello';     // Type 'string' is not assignable to type 'number'.
 }
 
 function getUpper(val: string) {
@@ -22,6 +22,9 @@ loginUser('prantik', '9tbs6@proton.me');
 
 export {}
 
-// now lets tweak fn addTwo. We'll change the return type to string and store the fn in a variable. We'll see this in the next part.
 
+// now lets tweak fn addTwo. We'll change the return type to string and store the fn in a variable. We'll see this in the next part.
+// => TWEAKED AND MENTIONED FN. addTwo() AS A number
+
+// Note:
 // Type inference is optional when we're decalring variables, but its VERY NECESSARY when we're defining functions.
