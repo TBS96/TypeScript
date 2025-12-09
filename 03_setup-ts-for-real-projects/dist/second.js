052f918190a8cc42eed45fca1e138057aede9c04
TBS96 <9tbs6@proton.me>
2025-12-09 13:16:45 +0530

    `interface` with `class` usecases
    - `class` always `implements` an interface and DOESNOT `extends` it. means it inherits all the properties of base class
    
    Signed-off-by: TBS96 <9tbs6@proton.me>

---
+++ 03_setup-ts-for-real-projects/dist/second.js
@@ -0,0 +1,22 @@
+"use strict";
+class Instagram {
+    constructor(cameraMode, filter, burst) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+}
+class Youtube {
+    constructor(cameraMode, filter, burst, short) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+        this.short = short;
+    }
+    createStory() {
+        console.log('Story was created');
+    }
+}
+const obj1 = new Youtube('cm', 'fil', 3, 'sh');
+console.log(obj1);
+console.log(obj1.createStory());
